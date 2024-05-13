ae65fbb38f78f564a3426865f2b3630f33e4d879
MrToastDux <tristanelibuss@icloud.com>
2024-05-13 21:58:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+var Cheese = 0;
+var autoclickercost = 10;
+var autoclickers = 0;
+var slavecost = 100;
+var slaves = 0;
+var robots = 0;
+var robotcost = 1000;
+var clickingpower = 1;
+var clickingpowercost = 100;
+var clickingpowerlevel = 1;
+
+
+function AddToScore(amount){
+    Cheese = Cheese + amount;
+    document.getElementById("Cheese").innerHTML = Cheese;
+}
+function buyAutoClicker(){
+    if (Cheese >= autoclickercost){
+        Cheese = Cheese - autoclickercost;
+        autoclickers = autoclickers + 1;
+        autoclickercost = Math.round(autoclickercost * 1.1);
+        document.getElementById("Cheese").innerHTML = Cheese;
+        document.getElementById("autoclickers").innerHTML = autoclickers;
+        document.getElementById("autoclickercost").innerHTML = autoclickercost;
+    }
+}
+setInterval (function(){
+    Cheese = Cheese + autoclickers *1;
+    document.getElementById("Cheese").innerHTML = Cheese;
+}, 5000);
+
+function buySlaves(){
+    if (Cheese >= slavecost){
+        Cheese = Cheese - slavecost;
+        slaves = slaves + 1;
+        slavecost = Math.round(slavecost * 1.1);
+        document.getElementById("Cheese").innerHTML = Cheese;
+        document.getElementById("slaves").innerHTML = slaves;
+        document.getElementById("slavecost").innerHTML = slavecost;
+    }
+}
+setInterval (function(){
+    Cheese = Cheese + slaves * 5;
+    document.getElementById("Cheese").innerHTML = Cheese;
+}, 2500);
+
+function buyRobots(){
+    if (Cheese >= robotcost){
+        Cheese = Cheese - robotcost;
+        robots = robots + 1;
+        robotcost = Math.round(robotcost * 1.1);
+        document.getElementById("Cheese").innerHTML = Cheese;
+        document.getElementById("robots").innerHTML = robots;
+        document.getElementById("robotcost").innerHTML = robotcost;
+    }
+}
+setInterval (function(){
+    Cheese = Cheese + robots * 10;
+    document.getElementById("Cheese").innerHTML = Cheese;
+    document.title = Cheese + "Cheese -- Clicker game"
+}, 1000);
+
+function buyClickingPower(){
+    if (Cheese >= clickingpowercost){
+        Cheese = Cheese - clickingpowercost;
+        clickingpowerlevel = clickingpowerlevel + 1;
+        clickingpower = clickingpower * 1000;
+        clickingpowercost = Math.round(clickingpowercost * 1);
+        document.getElementById("clickingpowercost").innerHTML = clickingpowercost;
+        document.getElementById("clickingpowerlevel").innerHTML = clickingpowerlevel;
+    }
+}
