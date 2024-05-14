c772b43059f99e3a1210005d2c0b93b2b23439d7
MrToastDux <tristanelibuss@icloud.com>
2024-05-14 12:39:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var slavecost = 100;
 var slaves = 0;
 var robots = 0;
 var robotcost = 1000;
+var grandfathers = 0;
+var grandfathercost = 10000;
 var clickingpower = 1;
 var clickingpowercost = 10000;
 var clickingpowerlevel = 1;
@@ -56,6 +58,22 @@ function buyRobots(){
 }
 setInterval (function(){
     Cheese = Cheese + robots * 10;
+    document.getElementById("Cheese").innerHTML = Cheese;
+    document.title = Cheese + "Cheese -- Clicker game"
+}, 1000);
+
+function buyGrandfathers(){
+    if (Cheese >= grandfathercost){
+        Cheese = Cheese - grandfathercost;
+        grandfathers = grandfathers + 1;
+        grandfathercost = Math.round(grandfathercost * 1.1);
+        document.getElementById("Cheese").innerHTML = Cheese;
+        document.getElementById("Grandfathers").innerHTML = grandfathers;
+        document.getElementById("Grandfatherscost").innerHTML = grandfathercost;
+    }
+}
+setInterval (function(){
+    Cheese = Cheese + grandfathers * 100;
     document.getElementById("Cheese").innerHTML = Cheese;
     document.title = Cheese + "Cheese -- Clicker game"
 }, 1000);
